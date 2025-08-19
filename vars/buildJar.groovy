99fb4c60fc7aba8a330e3dc8b063b39d15758e6e
Emmanuel Okoro <40013432+endiesworld@users.noreply.github.com>
2025-08-19 15:11:47 -0400

    Update buildJar.groovy

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,8 +1,8 @@
 #!/user/bin/env groovy
 
 def call() {
-    //echo "building the application for branch $BRANCH_NAME" // Only use for multi-branch job
-    //echo "building the application for branch $GIT_BRANCH"  // Only use for multi-branch job
+    echo "building the application for branch $BRANCH_NAME" // Only use for multi-branch job
+    echo "building the application for branch $GIT_BRANCH"  // Only use for multi-branch job
     echo "building the application ..."
     sh 'mvn package'
 }
